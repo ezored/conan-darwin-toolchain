572c9377325847d6707cc1c6af9583a643b4c97c
Paulo Coutinho <paulo@ubook.com>
2021-12-13 07:51:41 -0300

    add support for m1 and some refactoring

--- build.py
+++ build.py
@@ -5,7 +5,6 @@ if __name__ == "__main__":
     builder = ConanMultiPackager()
 
     builder.add(settings={"os": "Macos", "arch": "x86_64"})
-    builder.add(settings={"os": "Macos", "arch": "armv8"})
     builder.add(settings={"os": "iOS", "os.version": "9.0", "arch": "armv8.3"})
     builder.add(settings={"os": "watchOS", "os.version": "4.0", "arch": "armv7k"})
     builder.add(settings={"os": "tvOS", "os.version": "11.0", "arch": "armv8"})
